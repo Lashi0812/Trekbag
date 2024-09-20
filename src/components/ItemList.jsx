b825962a0df7bf05b1272f451b77a3b924a4c401
Lashi <lashi0812@gmail.com>
2024-09-20 09:24:40 +0000

    add sorting feature

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,56 @@
+import Select from "react-select";
+import { useMemo } from "react";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
+
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({ items, onToggleItem, handleDeleteItem }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = useMemo(
+    () =>
+      [...items].sort((a, b) => {
+        if (sortBy === "packed") {
+          return b.packed - a.packed;
+        }
+
+        if (sortBy === "unpacked") {
+          return a.packed - b.packed;
+        }
+
+        return;
+      }),
+    [items, sortBy]
+  );
+
   return (
     <ul className="item-list">
-      {items.length === 0 ? <EmptyView /> : null}
-      {items.map((item) => (
+      {sortedItems.length > 0 ? (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      ) : null}
+      {sortedItems.length === 0 ? <EmptyView /> : null}
+      {sortedItems.map((item) => (
         <Item
           key={item.id}
           item={item}
